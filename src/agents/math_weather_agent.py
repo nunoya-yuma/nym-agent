34af814c0dd0870c938b19feddb82073af242b41
nunoya-yuma <nuno.yu.3838@gmail.com>
2025-05-25 23:37:35 +0900

    Add the ability to search the web to the agent

--- src/agents/math_weather_agent.py
+++ src/agents/math_weather_agent.py
@@ -4,6 +4,8 @@ import os
 from pathlib import Path
 import argparse
 
+from langchain_community.tools.tavily_search import TavilySearchResults
+
 from src.common import agent_utils
 
 
@@ -32,12 +34,20 @@ async def main():
     agent.register_mcp_stdio("math", ["uv", "run", math_server_path])
     agent.register_mcp_streamable_http("weather", "http://localhost:8000/mcp")
 
+    search = TavilySearchResults(max_results=2)
+    tools = [search]
+    agent.register_normal_tool(tools)
+
     await agent.session()
     _ = await agent.send_query("what's (3 + 5) x 12")
     _ = await agent.send_query(
-        "I live in Osaka. Do you know where that is?")
+        "I live in Osaka. Do you know where that is?"
+        " (No need to search the web.)")
     _ = await agent.send_query(
         "What is the weather at my current location?")
+    _ = await agent.send_query(
+        "Research one of today's Japanese news"
+        " and tell me about it in Japanese.")
 
 
 if __name__ == "__main__":
